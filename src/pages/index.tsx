69c1153ef946bf1a0eb22253b687316f3ad017a2
Tetsu4819 <tetsu4819@gmail.com>
2021-05-16 17:07:18 +0900

    :hammer:Homeの整備（リンクの追加等）

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <p className="pt-16 text-6xl font-bold text-center">HelloNext</p>
+import Link from 'next/link'
+function Home() {
+  return (
+    <div>
+      <p className="pt-16 text-6xl font-bold text-center">HelloNext </p>
+      <p className="pt-8 text-4xl font-bold text-center">samples</p>
+      <ul className="text-center pt-4">
+        <li>
+          <Link href="counter">
+            <a className="pt-8 text-2xl font-bold text-center">counter</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
+
+export default Home
