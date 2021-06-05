f59b1192f0ded428834c645f13090de38c1e5ae8
Tetsu4819 <tetsu4819@gmail.com>
2021-06-05 10:58:56 +0900

    :fire:Remove purge-white-list from margin-ver

--- src/pages/escape/margin-ver.tsx
+++ src/pages/escape/margin-ver.tsx
@@ -29,23 +29,3 @@ export default function Grid() {
     </div>
   )
 }
-;[
-  'row-start-1',
-  'row-start-2',
-  'row-start-3',
-  'row-start-4',
-  'row-start-5',
-  'row-start-6',
-  'col-start-1',
-  'col-start-2',
-  'col-start-3',
-  'col-start-4',
-  'col-start-5',
-  'col-start-6',
-  'col-start-7',
-  'col-start-8',
-  'col-start-9',
-  'col-start-10',
-  'col-start-11',
-  'col-start-12'
-]
