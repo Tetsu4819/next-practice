8ae5383072228c1fd48d439b1fcd272c837b7d78
Tetsu4819 <tetsu4819@gmail.com>
2021-06-05 11:20:25 +0900

    :hammer:Fixed style of index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,29 +6,31 @@ function Home() {
       <Head>
         <title>Next Practice</title>
       </Head>
-      <h1 className="pt-16 text-6xl font-bold text-center">Next.js</h1>
-      <h2 className="pt-8 text-4xl font-bold text-center">samples</h2>
-      <ul className="pt-4 text-center">
-        <li>
-          <Link href="/counter">
-            <a className="mt-8 text-2xl font-bold text-center">counter</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/escape/grid-ver">
-            <a className="mt-8 text-2xl font-bold text-center">
-              Escape button(grid)
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/escape/margin-ver">
-            <a className="mt-8 text-2xl font-bold text-center">
-              Escape button(margin)
-            </a>
-          </Link>
-        </li>
-      </ul>
+      <div>
+        <h1 className="pt-16 text-6xl font-bold text-center">Next.js</h1>
+        <h2 className="pt-8 text-4xl font-bold text-center">samples</h2>
+        <ul className="pt-4 text-center">
+          <li>
+            <Link href="/counter">
+              <a className="mt-8 text-2xl font-bold text-center">counter</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/escape/grid-ver">
+              <a className="mt-8 text-2xl font-bold text-center">
+                Escape button(grid)
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/escape/margin-ver">
+              <a className="mt-8 text-2xl font-bold text-center">
+                Escape button(margin)
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
   )
 }
